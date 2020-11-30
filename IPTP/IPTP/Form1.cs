75252964ef81b43b91858edeb5eb62b63bfe24da
outstanding1301 <outstanding1301@gmail.com>
2020-11-30 16:21:53 +0900

    컴포넌트 생성 및 이벤트 작성중

--- IPTP/IPTP/Form1.cs
+++ IPTP/IPTP/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
 
 namespace IPTP
 {
@@ -15,6 +17,34 @@ namespace IPTP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String filePath = openFile();
+            if(filePath != null)
+            {
+                Mat screen = null, find = null, res = null;
+                screen = OpenCvSharp.Extensions.BitmapConverter.ToMat((Bitmap)pictureBox1.Image);
+                find = OpenCvSharp.Extensions.BitmapConverter.ToMat(new Bitmap(filePath));
+            }
+        }
+
+        private String openFile()
+        {
+            String filePath = null;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "이미지 파일 (*.jpg)|*.jpg|(*.png)|*.png|모든 파일 (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+            }
+            return filePath;
         }
     }
 }
