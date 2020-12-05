473ede552acf1e010dd35eb23f21e8d7cb4f9b8f
jo631 <52406729+jo631@users.noreply.github.com>
2020-12-05 19:14:04 +0900

    블러와 샤프닝 폼 init

---
+++ IPTP/IPTP/BlurNSharpeningForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPTP
+{
+    public partial class BlurNSharpeningForm : Form
+    {
+        private Form1 form;
+        public BlurNSharpeningForm(Form1 form)
+        {
+            InitializeComponent();
+            this.form = form;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
