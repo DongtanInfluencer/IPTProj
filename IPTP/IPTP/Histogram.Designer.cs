46f85cbc09ca17a21b3016761c4e970475a6a9cf
jo631 <52406729+jo631@users.noreply.github.com>
2020-12-05 16:26:18 +0900

    흑백히스토그램 제거, 히스토그램 표시 자동화(버튼삭제)

--- IPTP/IPTP/Histogram.Designer.cs
+++ IPTP/IPTP/Histogram.Designer.cs
@@ -28,31 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pb_Histogram_grayScale = new System.Windows.Forms.PictureBox();
             this.pb_Histogram_Red = new System.Windows.Forms.PictureBox();
             this.pb_Histogram_Green = new System.Windows.Forms.PictureBox();
             this.pb_Histogram_Blue = new System.Windows.Forms.PictureBox();
-            this.Btn_HIstogramShow = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_grayScale)).BeginInit();
+            this.Btn_HistogramEqual = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Red)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Green)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Blue)).BeginInit();
             this.SuspendLayout();
             // 
-            // pb_Histogram_grayScale
-            // 
-            this.pb_Histogram_grayScale.BackColor = System.Drawing.Color.White;
-            this.pb_Histogram_grayScale.Location = new System.Drawing.Point(28, 47);
-            this.pb_Histogram_grayScale.Name = "pb_Histogram_grayScale";
-            this.pb_Histogram_grayScale.Size = new System.Drawing.Size(150, 150);
-            this.pb_Histogram_grayScale.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pb_Histogram_grayScale.TabIndex = 0;
-            this.pb_Histogram_grayScale.TabStop = false;
-            // 
             // pb_Histogram_Red
             // 
             this.pb_Histogram_Red.BackColor = System.Drawing.Color.White;
-            this.pb_Histogram_Red.Location = new System.Drawing.Point(229, 47);
+            this.pb_Histogram_Red.Location = new System.Drawing.Point(21, 29);
             this.pb_Histogram_Red.Name = "pb_Histogram_Red";
             this.pb_Histogram_Red.Size = new System.Drawing.Size(150, 150);
             this.pb_Histogram_Red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -62,46 +50,45 @@
             // pb_Histogram_Green
             // 
             this.pb_Histogram_Green.BackColor = System.Drawing.Color.White;
-            this.pb_Histogram_Green.Location = new System.Drawing.Point(429, 47);
+            this.pb_Histogram_Green.Location = new System.Drawing.Point(221, 29);
             this.pb_Histogram_Green.Name = "pb_Histogram_Green";
             this.pb_Histogram_Green.Size = new System.Drawing.Size(150, 150);
             this.pb_Histogram_Green.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_Histogram_Green.TabIndex = 2;
             this.pb_Histogram_Green.TabStop = false;
+            this.pb_Histogram_Green.Click += new System.EventHandler(this.pb_Histogram_Green_Click);
             // 
             // pb_Histogram_Blue
             // 
             this.pb_Histogram_Blue.BackColor = System.Drawing.Color.White;
-            this.pb_Histogram_Blue.Location = new System.Drawing.Point(624, 47);
+            this.pb_Histogram_Blue.Location = new System.Drawing.Point(416, 29);
             this.pb_Histogram_Blue.Name = "pb_Histogram_Blue";
             this.pb_Histogram_Blue.Size = new System.Drawing.Size(150, 150);
             this.pb_Histogram_Blue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_Histogram_Blue.TabIndex = 3;
             this.pb_Histogram_Blue.TabStop = false;
             // 
-            // Btn_HIstogramShow
+            // Btn_HistogramEqual
             // 
-            this.Btn_HIstogramShow.Location = new System.Drawing.Point(28, 268);
-            this.Btn_HIstogramShow.Name = "Btn_HIstogramShow";
-            this.Btn_HIstogramShow.Size = new System.Drawing.Size(150, 33);
-            this.Btn_HIstogramShow.TabIndex = 4;
-            this.Btn_HIstogramShow.Text = "히스토그램 표시";
-            this.Btn_HIstogramShow.UseVisualStyleBackColor = true;
-            this.Btn_HIstogramShow.Click += new System.EventHandler(this.Btn_ShowHistogram);
+            this.Btn_HistogramEqual.Location = new System.Drawing.Point(21, 205);
+            this.Btn_HistogramEqual.Name = "Btn_HistogramEqual";
+            this.Btn_HistogramEqual.Size = new System.Drawing.Size(150, 33);
+            this.Btn_HistogramEqual.TabIndex = 5;
+            this.Btn_HistogramEqual.Text = "히스토그램 평탄화";
+            this.Btn_HistogramEqual.UseVisualStyleBackColor = true;
+            this.Btn_HistogramEqual.Click += new System.EventHandler(this.Btn_HistogramEqual_Click);
             // 
             // Histogram
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 326);
-            this.Controls.Add(this.Btn_HIstogramShow);
+            this.ClientSize = new System.Drawing.Size(588, 249);
+            this.Controls.Add(this.Btn_HistogramEqual);
             this.Controls.Add(this.pb_Histogram_Blue);
             this.Controls.Add(this.pb_Histogram_Green);
             this.Controls.Add(this.pb_Histogram_Red);
-            this.Controls.Add(this.pb_Histogram_grayScale);
             this.Name = "Histogram";
             this.Text = "Histogram";
-            ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_grayScale)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Red)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Green)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_Histogram_Blue)).EndInit();
@@ -110,11 +97,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pb_Histogram_grayScale;
         private System.Windows.Forms.PictureBox pb_Histogram_Red;
         private System.Windows.Forms.PictureBox pb_Histogram_Green;
         private System.Windows.Forms.PictureBox pb_Histogram_Blue;
-        private System.Windows.Forms.Button Btn_HIstogramShow;
+        private System.Windows.Forms.Button Btn_HistogramEqual;
     }
 }
--- IPTP/IPTP/Histogram.cs
+++ IPTP/IPTP/Histogram.cs
@@ -1,4 +1,5 @@
 ﻿using OpenCvSharp;
+using System;
 using System.Windows.Forms;
 
 namespace IPTP
@@ -6,37 +7,31 @@ namespace IPTP
     public partial class Histogram : Form
     {
         private Form1 form = null;
-        private Mat src = null;
         private Mat dst = null;
+        private Mat history = null;
 
         public Histogram(Form1 form)
         {
             InitializeComponent();
             this.form = form;
-
-            
         }
 
-        private void Btn_ShowHistogram(object sender, System.EventArgs e)
+        protected override void OnActivated(EventArgs e)
         {
-            src = form.getSrc();
+            //폼이 켜질때 자동으로 히스토그램 표출 , 켜질때의 Dst를 history로 표시
+            base.OnActivated(e);
             dst = form.getDst();
+            updateHistogram(dst);
+            history = dst.Clone();
+        }
 
-            Mat gray = new Mat(dst.Size(), MatType.CV_8UC1);
-            Cv2.CvtColor(dst, gray, ColorConversionCodes.BGR2GRAY);
-            var rgb = Cv2.Split(dst);
-            //Mat[] rgb = new Mat[3];
-            //Cv2.Split(dst, out rgb);
+        private void updateHistogram(Mat dst)
+        {
+            Mat[] rgb = Cv2.Split(dst);
 
-            pb_Histogram_grayScale.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(MatToHistogram(gray));
             pb_Histogram_Blue.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(MatToHistogram(rgb[0]));
             pb_Histogram_Green.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(MatToHistogram(rgb[1]));
             pb_Histogram_Red.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(MatToHistogram(rgb[2]));
-            //pb_Histogram_grayScale.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(gray);
-            //pb_Histogram_Blue.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[0]);
-            //pb_Histogram_Green.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[1]);
-            //pb_Histogram_Red.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[2]);
-
 
         }
         private Mat MatToHistogram(Mat histogram)
@@ -50,7 +45,7 @@ namespace IPTP
             int[] hdims = { 256 }; // Histogram size for each dimension
             Rangef[] ranges = { new Rangef(0, 256), }; // min/max
             Cv2.CalcHist(
-                new Mat[] { src },
+                new Mat[] { histogram },
                 new int[] { 0 },
                 null,
                 hist,
@@ -70,12 +65,32 @@ namespace IPTP
                 int binW = (int)((double)Width / hdims[0]);
                 render.Rectangle(
                     new OpenCvSharp.Point(j * binW, render.Rows - (int)(hist.Get<float>(j))),
-                    new OpenCvSharp.Point((j + 1) * binW, render.Rows),
-                    color,
-                    -1);
+                    new OpenCvSharp.Point((j + 1) * binW, render.Rows),color,-1);
             }
 
             return render;
+        }
+
+        private void Btn_HistogramEqual_Click(object sender, System.EventArgs e)
+        {
+            dst = form.getDst();
+
+            Mat[] rgb = Cv2.Split(dst);
+            //색깔별 평탄화
+            Cv2.EqualizeHist(rgb[0], rgb[0]);
+            Cv2.EqualizeHist(rgb[1], rgb[1]);
+            Cv2.EqualizeHist(rgb[2], rgb[2]);
+            Cv2.Merge(rgb, dst);
+
+            form.updateDst();
+            updateHistogram(form.getDst());
+
+
+        }
+
+        private void pb_Histogram_Green_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
