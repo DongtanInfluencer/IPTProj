6df37c0a83fec9c6503d2b76941b257f2d9fd52a
jo631 <52406729+jo631@users.noreply.github.com>
2020-12-07 23:38:40 +0900

    rgb 8UC1 -> 8UC3 변환

--- IPTP/IPTP/ColorForm.cs
+++ IPTP/IPTP/ColorForm.cs
@@ -28,7 +28,24 @@ namespace IPTP
             Mat hsvSrc = src.Clone();
             Cv2.CvtColor(src, hsvSrc, ColorConversionCodes.BGR2HSV);
             Mat[] rgb = Cv2.Split(src), hsv=Cv2.Split(hsvSrc);
+
+            Mat blankMat = new Mat(src.Size(), MatType.CV_8UC1);
             
+            Mat[] red = new Mat[3];
+            red[0] = red[1] = blankMat;
+            red[2] = rgb[2];
+            Cv2.Merge(red, rgb[2]);
+
+            Mat[] green = new Mat[3];
+            green[0] = green[2] = blankMat;
+            green[1] = rgb[1];
+            Cv2.Merge(green, rgb[1]);
+
+            Mat[] blue = new Mat[3];
+            blue[1] = blue[2] = blankMat;
+            blue[0] = rgb[0];
+            Cv2.Merge(blue, rgb[0]);
+
             pb_image_Red.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[2]);
             pb_image_Green.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[1]);
             pb_image_Blue.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[0]);
