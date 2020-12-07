f3716040a409244f1c7e29355895c3132e812cb3
jo631 <52406729+jo631@users.noreply.github.com>
2020-12-07 23:22:39 +0900

    이미지 추출한거 뷰로 보여줌

--- IPTP/IPTP/ColorForm.Designer.cs
+++ IPTP/IPTP/ColorForm.Designer.cs
@@ -28,20 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pb_image_Red = new System.Windows.Forms.PictureBox();
+            this.pb_image_Green = new System.Windows.Forms.PictureBox();
+            this.pb_image_Blue = new System.Windows.Forms.PictureBox();
+            this.pb_image_Hue = new System.Windows.Forms.PictureBox();
+            this.pb_image_Sat = new System.Windows.Forms.PictureBox();
+            this.pb_image_Value = new System.Windows.Forms.PictureBox();
+            this.Btn_HistogramEqual = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Red)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Green)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Hue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Sat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Value)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pb_image_Red
+            // 
+            this.pb_image_Red.BackColor = System.Drawing.Color.White;
+            this.pb_image_Red.Location = new System.Drawing.Point(12, 12);
+            this.pb_image_Red.Name = "pb_image_Red";
+            this.pb_image_Red.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Red.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Red.TabIndex = 2;
+            this.pb_image_Red.TabStop = false;
+            // 
+            // pb_image_Green
+            // 
+            this.pb_image_Green.BackColor = System.Drawing.Color.White;
+            this.pb_image_Green.Location = new System.Drawing.Point(168, 12);
+            this.pb_image_Green.Name = "pb_image_Green";
+            this.pb_image_Green.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Green.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Green.TabIndex = 3;
+            this.pb_image_Green.TabStop = false;
+            // 
+            // pb_image_Blue
+            // 
+            this.pb_image_Blue.BackColor = System.Drawing.Color.White;
+            this.pb_image_Blue.Location = new System.Drawing.Point(324, 12);
+            this.pb_image_Blue.Name = "pb_image_Blue";
+            this.pb_image_Blue.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Blue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Blue.TabIndex = 4;
+            this.pb_image_Blue.TabStop = false;
+            // 
+            // pb_image_Hue
+            // 
+            this.pb_image_Hue.BackColor = System.Drawing.Color.White;
+            this.pb_image_Hue.Location = new System.Drawing.Point(12, 190);
+            this.pb_image_Hue.Name = "pb_image_Hue";
+            this.pb_image_Hue.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Hue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Hue.TabIndex = 5;
+            this.pb_image_Hue.TabStop = false;
+            // 
+            // pb_image_Sat
+            // 
+            this.pb_image_Sat.BackColor = System.Drawing.Color.White;
+            this.pb_image_Sat.Location = new System.Drawing.Point(168, 190);
+            this.pb_image_Sat.Name = "pb_image_Sat";
+            this.pb_image_Sat.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Sat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Sat.TabIndex = 6;
+            this.pb_image_Sat.TabStop = false;
+            // 
+            // pb_image_Value
+            // 
+            this.pb_image_Value.BackColor = System.Drawing.Color.White;
+            this.pb_image_Value.Location = new System.Drawing.Point(324, 190);
+            this.pb_image_Value.Name = "pb_image_Value";
+            this.pb_image_Value.Size = new System.Drawing.Size(150, 150);
+            this.pb_image_Value.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_image_Value.TabIndex = 7;
+            this.pb_image_Value.TabStop = false;
+            // 
+            // Btn_HistogramEqual
+            // 
+            this.Btn_HistogramEqual.Location = new System.Drawing.Point(486, 12);
+            this.Btn_HistogramEqual.Name = "Btn_HistogramEqual";
+            this.Btn_HistogramEqual.Size = new System.Drawing.Size(150, 33);
+            this.Btn_HistogramEqual.TabIndex = 8;
+            this.Btn_HistogramEqual.Text = "히스토그램 평탄화";
+            this.Btn_HistogramEqual.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(486, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "히스토그램 평탄화";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Location = new System.Drawing.Point(36, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 14);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Red";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Location = new System.Drawing.Point(195, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 14);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Green";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Location = new System.Drawing.Point(348, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 14);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "Blue";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox4.Location = new System.Drawing.Point(36, 346);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 14);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "Hue";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox5.Location = new System.Drawing.Point(195, 346);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 14);
+            this.textBox5.TabIndex = 14;
+            this.textBox5.Text = "Saturation";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox6.Location = new System.Drawing.Point(348, 346);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 14);
+            this.textBox6.TabIndex = 15;
+            this.textBox6.Text = "Value";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // ColorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(646, 367);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Btn_HistogramEqual);
+            this.Controls.Add(this.pb_image_Value);
+            this.Controls.Add(this.pb_image_Sat);
+            this.Controls.Add(this.pb_image_Hue);
+            this.Controls.Add(this.pb_image_Blue);
+            this.Controls.Add(this.pb_image_Green);
+            this.Controls.Add(this.pb_image_Red);
             this.Name = "ColorForm";
             this.Text = "ColorForm";
             this.Load += new System.EventHandler(this.ColorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Red)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Green)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Hue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Sat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_image_Value)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pb_image_Red;
+        private System.Windows.Forms.PictureBox pb_image_Green;
+        private System.Windows.Forms.PictureBox pb_image_Blue;
+        private System.Windows.Forms.PictureBox pb_image_Hue;
+        private System.Windows.Forms.PictureBox pb_image_Sat;
+        private System.Windows.Forms.PictureBox pb_image_Value;
+        private System.Windows.Forms.Button Btn_HistogramEqual;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
--- IPTP/IPTP/ColorForm.cs
+++ IPTP/IPTP/ColorForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,13 +14,41 @@ namespace IPTP
     public partial class ColorForm : Form
     {
         private Form1 form = null;
+        private Mat history = null;
         public ColorForm(Form1 form)
         {
             InitializeComponent();
             this.form = form;
         }
 
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Mat src = form.getSrc();
+            Mat hsvSrc = src.Clone();
+            Cv2.CvtColor(src, hsvSrc, ColorConversionCodes.BGR2HSV);
+            Mat[] rgb = Cv2.Split(src), hsv=Cv2.Split(hsvSrc);
+            
+            pb_image_Red.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[2]);
+            pb_image_Green.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[1]);
+            pb_image_Blue.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(rgb[0]);
+            pb_image_Hue.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(hsv[0]);
+            pb_image_Sat.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(hsv[1]);
+            pb_image_Value.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(hsv[2]);
+
+        }
+
         private void ColorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
